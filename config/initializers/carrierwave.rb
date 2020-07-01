99c6af686ae7a29b5afeb318caafaab6c0ee6abb
segawasho <segawasho0825pro@gmail.com>
2020-07-01 21:23:55 +0900

    s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_PRIVATE_KEY'],
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory  = 'limeapp-bucket'
+    config.asset_host = 'https://limeapp-bucket.s3.amazonaws.com'
+    config.cache_storage = :fog
+    config.fog_provider = 'fog/aws'
+
+  end
+end
