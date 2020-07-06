019eb68406b0e4b53f6bb4683c48c746877afc32
segawasho <segawasho0825pro@gmail.com>
2020-07-06 17:57:52 +0900

    heroku carrierwaverb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,22 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
+# 下記の三行は入れていた方がいいらしい
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
+      # Amazon S3用の設定
       provider: 'AWS',
+      region: ENV['AWS_S3_REGION'],
       aws_access_key_id: ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_PRIVATE_KEY'],
-      region: 'ap-northeast-1'
     }
-
-    config.fog_directory  = 'limeapp-bucket'
-    config.asset_host = 'https://limeapp-bucket.s3.amazonaws.com'
-    config.cache_storage = :fog
-    config.fog_provider = 'fog/aws'
-
+    config.fog_directory     =  'limeapp-bucket'
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
+
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
