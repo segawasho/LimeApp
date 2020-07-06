1c6a1595134d481d201fb4e5226d879644396ffe
segawasho <segawasho0825pro@gmail.com>
2020-07-06 17:59:36 +0900

    heroku carrierwaverb2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
-      region: ENV['AWS_S3_REGION'],
+      region: 'ap-northeast-1',
       aws_access_key_id: ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_PRIVATE_KEY'],
     }
