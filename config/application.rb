58998060ac4b612fa46d495b960c6a8b742aa51a
segawasho <segawasho0825pro@gmail.com>
2020-07-06 20:44:34 +0900

    fix productionrb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module LimeApp
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
