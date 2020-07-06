62a3090665bd6effc0e784ebf189a737b1c099d3
segawasho <segawasho0825pro@gmail.com>
2020-07-06 13:22:56 +0900

    little update

--- db/migrate/20200702133228_remove_images_at_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImagesAtProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :products, :images, :json
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_133228) do
+ActiveRecord::Schema.define(version: 2020_07_02_125610) do
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_133228) do
     t.integer "stock_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "images"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
